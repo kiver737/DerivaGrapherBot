0103e6d4fd23626bb0a87d6b88fd7857e58cda83
vladislav <perfiljevkiver@yandex.ru>
2024-04-12 16:36:37 +0300

    🚀 Initial commit: Launching the Telegram Bot project!

---
+++ bot.py
@@ -0,0 +1,3 @@
+from handlers import bot
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
---
+++ config.py
@@ -0,0 +1,5 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+ADMIN_ID = int(os.getenv('ADMIN_ID', '0'))
